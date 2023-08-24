359fc9570c97f89e8733be832d9e2a3d7043ed75
Toby Milner-Gulland <tmgulland@movingbrands.com>
2023-08-24 17:21:09 +0100

    added sticky scrolling to parcel and grocery pages

--- app/site/industry-solutions-grocery/page.tsx
+++ app/site/industry-solutions-grocery/page.tsx
@@ -39,13 +39,42 @@ const Page = () => {
       </Section>
       <Section inset>
         <Title headline="Key features" />
-        <Placeholder animated>
+        <Placeholder style={{
+          position: 'sticky',
+          top: 100,
+          background: "rgb(220,220,220)",
+          aspectRatio: `2.5 / 1`,
+          zIndex: 1,
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+          boxShadow: `0px 4px 19px 0px rgba(0, 0, 0, 0.10)`
+        }} >
           <h1 className={h3}>Key feature 1</h1>
         </Placeholder>
-        <Placeholder animated>
+        <Placeholder style={{
+          position: 'sticky',
+          top: 100,
+          transform: 'translateY(80px)',
+          background: "rgb(190,190,190)",
+          aspectRatio: `2.5 / 1`,
+          zIndex: 2,
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+          boxShadow: `0px 4px 19px 0px rgba(0, 0, 0, 0.10)`
+        }} >
           <h1 className={h3}>Key feature 2</h1>
         </Placeholder>
-        <Placeholder animated>
+        <Placeholder style={{
+          position: 'sticky',
+          top: 100,
+          transform: 'translateY(130px)',
+          background: "rgb(160,160,160)",
+          aspectRatio: `2.5 / 1`,
+          zIndex: 3,
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+          boxShadow: `0px 4px 19px 0px rgba(0, 0, 0, 0.10)`
+        }} >
           <h1 className={h3}>Key feature 3</h1>
         </Placeholder>
       </Section>
